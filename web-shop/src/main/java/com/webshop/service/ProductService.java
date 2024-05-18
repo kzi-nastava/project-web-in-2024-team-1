89738326f357105fe7660982604ddd61b38bce6b
Teodora Jovic <teodorajovic2002@gmail.com>
2024-05-18 20:47:39 +0200

    added logic in service

--- web-shop/src/main/java/com/webshop/service/ProductService.java
+++ web-shop/src/main/java/com/webshop/service/ProductService.java
@@ -3,6 +3,8 @@ import com.webshop.model.Product;
 import com.webshop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,12 +22,16 @@ public class ProductService {
         return null;
     }
 
-//    public Product findByDescription(String productDescription) {
-//        Optional<Product> product = productRepository.findByDescription(productDescription);
-//        if(product.isPresent()){
-//            return product.get();
-//        }
-//        return null;
-//    }
+    public Product findByDescription(String productDescription) {
+        Optional<Product> product = productRepository.findByDescription(productDescription);
+        if(product.isPresent()){
+            return product.get();
+        }
+        return null;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
 
 }
