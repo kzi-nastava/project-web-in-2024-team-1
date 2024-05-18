46bb2cc88920d8906b6cfa65e14502f41f331861
Stasa Grajic <stasa26grajic@gmail.com>
2024-05-18 19:26:11 +0200

    pocetak

--- web-shop/src/main/resources/data.sql
+++ web-shop/src/main/resources/data.sql
@@ -1,8 +1,10 @@
 
 
-INSERT INTO ACCOUNT (name,last_name,username,email,phone_number,password,date_of_birth,image_path,description,user_role,average_rating) VALUES ('Ana','Andric','ana123','Ana.Andric@gmail.com','0664536780','ana111','2001-02-10','datoteka.jpg','bio','SELLER',10.0);
-INSERT INTO ACCOUNT (name,last_name,username,email,phone_number,password,date_of_birth,image_path,description,user_role,average_rating) VALUES ('Maja','Majic','maja123','Maja.Majic@gmail.com','0624536780','maja111','2002-02-10','datoteka.jpg','bio','CUSTOMER',5.0);
-INSERT INTO ACCOUNT (name,last_name,username,email,phone_number,password,date_of_birth,image_path,description,user_role,average_rating) VALUES ('Jovan','Jovic','jovan123','Jovan.Jovic@gmail.com','0654536780','jovan111','1999-02-10','datoteka.jpg','bio','ADMINISTRATOR',9.6);
+INSERT INTO ACCOUNT (name,last_name,username,email,phone_number,password,date_of_birth,image_path,description,user_role,average_rating)
+VALUES
+    ('Ana','Andric','ana123','Ana.Andric@gmail.com','0664536780','ana111','2001-02-10','datoteka.jpg','bio','SELLER',10.0),
+    ('Maja','Majic','maja123','Maja.Majic@gmail.com','0624536780','maja111','2002-02-10','datoteka.jpg','bio','CUSTOMER',5.0),
+    ('Jovan','Jovic','jovan123','Jovan.Jovic@gmail.com','0654536780','jovan111','1999-02-10','datoteka.jpg','bio','ADMINISTRATOR',9.6);
 
 INSERT INTO OFFER (price_offer, account_id)VALUES (20.00, 1),(40.00, 2),(30.00, 3);
 
@@ -21,10 +23,20 @@ INSERT INTO PRODUCT_OFFERS (product_id, offer_id) VALUES (1, 1),(3,2),(2,3);
 --INSERT INTO OFFER (price_offer, account_id)VALUES (30.00, 1);
 --INSERT INTO CATEGORY (category_name) VALUES ('ime kategorije')
 
+INSERT INTO ACCOUNT_PRODUCT (account_id,product_id) VALUES (1,1);
 
-INSERT INTO REVIEW (rating,comment,review_date,user_id) VALUES (5,'komentar','2024-01-24',2);
-INSERT INTO REVIEW (rating,comment,review_date,user_id) VALUES (3,'komentar','2024-01-04',1);
 
-INSERT INTO REPORT_USER (reason,report_date,reporting_id,reporter_id) VALUES ('nesto','2024-04-12',1,2);
-INSERT INTO REPORT_USER (reason,report_date,reporting_id,reporter_id) VALUES ('nesto','2024-04-10',2,1);
+INSERT INTO REVIEW (rating,comment,review_date,user_id)
+VALUES
+    (5,'komentar','2024-01-24',2),
+    (3,'komentar','2024-01-04',1);
+
+INSERT INTO ACCOUNT_REVIEW (account_id,review_id)
+VALUES
+    (1,1),(2,2);
+
+INSERT INTO REPORT_USER (reason,report_date,reporting_id,reporter_id)
+VALUES
+    ('nesto','2024-04-12',1,2),
+    ('nesto','2024-04-10',2,1);
 
