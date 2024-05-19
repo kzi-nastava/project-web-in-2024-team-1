f6d8da71102dd519e9845c709b8433e475d1046c
Teodora Jovic <teodorajovic2002@gmail.com>
2024-05-19 22:00:23 +0200

    adding ProductController

---
+++ web-shop/src/main/java/com/webshop/controller/ProductController.java
@@ -0,0 +1,57 @@
+package com.webshop.controller;
+
+import com.webshop.dto.AccountDto;
+import com.webshop.dto.ProductDto;
+import com.webshop.model.Account;
+import com.webshop.model.Product;
+import com.webshop.service.ProductService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/")
+public class ProductController
+{
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getAllProducts(HttpSession session)
+    {
+        List<Product> productList = productService.findAll();
+
+        Product product1 = (Product) session.getAttribute("product");
+        if(product1 == null){System.out.println("Product not found");}
+
+        else {System.out.println("Product found");}
+
+        List<ProductDto> productDtoList = new ArrayList<>();
+
+        for(Product product : productList)
+        {
+            ProductDto productDto = new ProductDto(product);
+            productDtoList.add(productDto);
+        }
+
+        return ResponseEntity.ok(productDtoList);
+    }
+
+    @GetMapping("product/{id}")
+     public ResponseEntity<Product>getProductById(@PathVariable("id") Long id)
+     {
+         Product product = productService.findOne(id);
+         if(product == null) {return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);}
+
+         ProductDto productDto = new ProductDto(product);
+         return ResponseEntity.ok(product);
+     }
+}
