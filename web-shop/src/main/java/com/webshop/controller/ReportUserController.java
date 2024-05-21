0eb04998ef5f00a98f148fc9e0efb2a4e6942bfd
Stasa Grajic <stasa26grajic@gmail.com>
2024-05-21 17:31:26 +0200

    report

--- web-shop/src/main/java/com/webshop/controller/ReportUserController.java
+++ web-shop/src/main/java/com/webshop/controller/ReportUserController.java
@@ -1,7 +1,10 @@
 package com.webshop.controller;
 
+import com.webshop.dto.AccountDto;
+import com.webshop.model.Account;
 import com.webshop.dto.ReportUserDto;
 import com.webshop.service.ReportUserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,5 +28,33 @@ public class ReportUserController {
     @GetMapping("all")
     public List<ReportUserDto> getAllReports() {
         return reportUserService.getAllReportUsers();
+    }
+
+    @PostMapping("accept/{reportId}")
+    public ResponseEntity<String> acceptReport(@PathVariable Long reportId, HttpSession session) {
+        Account account = (Account) session.getAttribute("account");
+        if (account == null) {
+            return new ResponseEntity<>("Not logged in", HttpStatus.UNAUTHORIZED);
+        }
+        try{
+            reportUserService.acceptReport(reportId,account);
+            return ResponseEntity.ok("Report accepted");
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("reject/{reportId}")
+    public ResponseEntity<String> rejectReport(@PathVariable Long reportId,@RequestParam String rejectReason,HttpSession session) {
+        Account account = (Account) session.getAttribute("account");
+        if (account == null) {
+            return new ResponseEntity<>("Not logged in", HttpStatus.UNAUTHORIZED);
+        }
+        try{
+            reportUserService.rejectReport(reportId,rejectReason,account);
+            return ResponseEntity.ok("Report rejected");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
