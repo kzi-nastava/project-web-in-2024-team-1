0958433b8222a91d00802ae1bae0e86234ce7229
Teodora Jovic <teodorajovic2002@gmail.com>
2024-05-22 19:44:56 +0200

    adding offer and purchase dto

---
+++ web-shop/src/main/java/com/webshop/dto/PurchaseProductDto.java
@@ -0,0 +1,58 @@
+package com.webshop.dto;
+
+import com.webshop.model.Product;
+
+public class PurchaseProductDto {
+    private String productName;
+    private Double price;
+    private String imagePath;
+    private Boolean isSold=true;
+
+    public PurchaseProductDto() { }
+
+    public PurchaseProductDto(String productName, Double price, String imagePath, Boolean isSold) {
+        this.productName = productName;
+        this.price = price;
+        this.imagePath = imagePath;
+        this.isSold = isSold;
+    }
+
+    public PurchaseProductDto(Product product) {
+        this.productName = product.getName();
+        this.price = product.getPrice();
+        this.imagePath = product.getImagePath();
+        this.isSold=product.getSold();
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public Boolean getSold() {
+        return isSold;
+    }
+
+    public void setSold(Boolean sold) {
+        isSold = sold;
+    }
+}
