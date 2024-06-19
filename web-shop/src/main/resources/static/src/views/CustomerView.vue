658354e3e291abbce6a4bc8d50198ae6c8b9ad6f
Teodora Jovic <teodorajovic2002@gmail.com>
2024-06-19 18:21:07 +0200

    customer and seller vue

--- web-shop/src/main/resources/static/src/views/CustomerView.vue
+++ web-shop/src/main/resources/static/src/views/CustomerView.vue
@@ -1,2 +1,18 @@
-<template></template>
-<script></script>
+<template>
+    <div class="customer">
+   
+       <Customer />
+     </div>
+   </template>
+   <script>
+   
+   // @ is an alias to /src
+   import Customer from '@/components/Customer.vue'
+   
+   export default {
+     name: 'CustomerView',
+     components: {
+        Customer
+     }
+   }
+   </script>
--- web-shop/src/main/resources/static/src/views/SellerView.vue
+++ web-shop/src/main/resources/static/src/views/SellerView.vue
@@ -1,2 +1,18 @@
-<template></template>
-<script></script>
+<template>
+    <div class="seller">
+   
+       <Seller />
+     </div>
+   </template>
+   <script>
+   
+   // @ is an alias to /src
+   import Seller from '@/components/Seller.vue'
+   
+   export default {
+     name: 'SellerView',
+     components: {
+        Seller
+     }
+   }
+   </script>
